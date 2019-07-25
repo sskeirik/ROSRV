022f90de11dbee15c2c6a24853e7b1fb64ff78df
Nishant Rodrigues <nishantjr@gmail.com>
2019-07-25 16:04:14 -0500

    Test,src/RVMaster/src/test.cpp: test for publish/subscribe to unmonitored channel
    Running this test requires rvmaster to be running

--- src/RVMaster/src/test.cpp
+++ src/RVMaster/src/test.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
+#include <string>
+#include <boost/optional.hpp>
 
-void test_monitor()
+#include "rvmonitor.h"
+
+void test_unmonitored_channel()
 {
-    throw std::runtime_error("Oops.");
+    int argc = 1; char* argv[] = { "test" };
+    char const * node_name = __FUNCTION__;
+    ros::init(argc, argv, node_name);
+    ros::NodeHandle n;
+    ros::Rate rate(10);
+
+    string const topic = "unmonitored";
+
+    ros::Publisher pub = n.advertise<std_msgs::String>(topic, 1000);
+    ros::spinOnce();
+
+    boost::optional<std_msgs::String> msg_recvd;
+    auto recieved_callback =
+        [&msg_recvd] (std_msgs::String::ConstPtr const& msg) {
+            msg_recvd = * msg;
+        };
+    ros::Subscriber sub = n.subscribe<std_msgs::String>(topic, 1000, recieved_callback);
+    ros::spinOnce();
+
+    char const* msg_data = "Hello from publisher!\n";
+    std_msgs::String msg_sent;
+    msg_sent.data = msg_data;
+    pub.publish(msg_sent);
+    ros::spinOnce();
+
+    rate.sleep();
+    ros::spinOnce();
+
+    if (!msg_recvd) { throw std::runtime_error("Response not recieved!"); }
+    if (msg_recvd->data != msg_data) { throw std::runtime_error("Incorrect message data recieved"); }
 }
 
 int main()
 {
-    test_monitor();
+    test_unmonitored_channel();
     return 0;
 }
 
