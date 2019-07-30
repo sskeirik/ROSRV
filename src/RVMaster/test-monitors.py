69e067a47d4e7e8140c2b59f5d4d1c48b69a141c
Nishant Rodrigues <nishantjr@gmail.com>
2019-07-30 10:33:58 -0500

    Port C++ tests to python/pytest
    Messages sent between publishers and subscribers in the same process cannot be
    intercepted by rvmaster. This means that either one must be sent from an
    external process. This is much easier to do from python.

---
+++ src/RVMaster/test-monitors.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2
+
+import rospy as ros
+from subprocess import check_call
+from std_msgs.msg import String
+
+import pytest
+
+@pytest.fixture(scope="session")
+def ros_init():
+    ros.init_node('test')
+
+@pytest.fixture()
+def ros_subscribe(ros_init):
+    def subscribe(topic):
+        class context: # see https://stackoverflow.com/a/28433571/1278288
+            recieved = []
+        def callback(msg):
+            ros.loginfo("Recieved: %s", msg.data)
+            context.recieved += [msg.data]
+        rate = ros.Rate(10) # Hz
+        ros.Subscriber(topic, String, callback)
+        rate.sleep()
+        return [ros_init, rate, context.recieved]
+    return subscribe
+
+def test_unmonitored_channel(ros_subscribe):
+    [ros_init, rate, recieved_messages] = ros_subscribe('unmonitored') 
+    check_call(['rostopic', 'pub', '--once', '/unmonitored', 'std_msgs/String', 'Hi!'])
+    rate.sleep(); rate.sleep(); rate.sleep(); rate.sleep()
+    assert(recieved_messages == ['Hi!'])
+
+def test_monitored_channel(ros_subscribe):
+    [ros_init, rate, recieved_messages] = ros_subscribe('chatter') 
+    check_call(['rostopic', 'pub', '--once', '/chatter', 'std_msgs/String', 'Hi!'])
+    rate.sleep(); rate.sleep(); rate.sleep(); rate.sleep()
+    assert(recieved_messages == ['Hi!RV'])
+
