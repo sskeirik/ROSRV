8b8dc1ca78f0eeaa91e5148fcd0ca7772f70476c
Manasvi Saxena <msaxena2@illinois.edu>
2019-09-12 19:05:02 -0700

    t/test-without-rvmaster: Simplify Infrastructure using parametric fixtures

--- t/test-without-rvmaster.py
+++ t/test-without-rvmaster.py
@@ -6,79 +6,93 @@ from std_msgs.msg import String, ColorRGBA
 
 import pytest
 
+class RosTestPacket(object):
+    def __init__(self, topic_name, msg_class, msg_type, to_send, prefix):
+        self.topic_name = topic_name
+        self.msg_class = msg_class
+        self.msg_type = msg_type
+        self.to_send = to_send
+        self.recieved = []
+        self.prefix = prefix
+
+    def send_topic(self):
+            return self.prefix + self.topic_name
+
+    def to_formatted_string(self, msg):
+        pass
+
+    def callback(self, msg):
+        self.recieved += [self.to_formatted_string(msg)]
+
+class StringPacket(RosTestPacket):
+    def __init__(self, topic_name, to_send, prefix):
+        super(StringPacket, self).__init__(topic_name, String, 'std_msgs/String', to_send, prefix)
+
+    def to_formatted_string(self, msg):
+        return msg.data
+
+
+class ColorRGBAPacket(RosTestPacket):
+    def __init__(self, topic_name, to_send, prefix):
+        super(ColorRGBAPacket, self).__init__(topic_name, ColorRGBA, 'std_msgs/ColorRGBA', to_send, prefix)
+
+    def to_formatted_string(self, msg):
+        return '({},{},{},{})'.format(msg.r, msg.g, msg.b, msg.a)
+
+# Fixtures
 @pytest.fixture(scope="session")
 def ros_init():
     ros.init_node('test')
 
 @pytest.fixture()
-def ros_subscribe(ros_init):
-    def subscribe(topic, msg_type, to_formatted_string):
-        class context: # see https://stackoverflow.com/a/28433571/1278288
-            recieved = []
-        def callback(msg):
-            recieved_str = to_formatted_string(msg)
-            ros.loginfo("Recieved: %s", recieved_str)
-            context.recieved += [recieved_str]
-        rate = ros.Rate(10) # Hz
-        ros.Subscriber(topic, msg_type, callback)
-        rate.sleep()
-        return [ros_init, rate, context.recieved]
-    return subscribe
+def simple_pipeline(ros_init):
+    def run_simple_pipeline(test_packets):
+        rate = ros.Rate(10);
+        for packet in test_packets:
+            # register callbacks
+            ros.Subscriber(packet.topic_name, packet.msg_class, packet.callback)
+            rate.sleep()
+            for msg in packet.to_send:
+                check_call(['rostopic', 'pub', '--once', packet.send_topic(), packet.msg_type, msg])
+            rate.sleep(); rate.sleep(); rate.sleep(); rate.sleep()
+    return run_simple_pipeline
 
-@pytest.fixture()
+@pytest.fixture(params=[True, False], ids=['monitor_running', 'no_monitor_running'])
 def launch_monitor(request):
-    def launch(name):
-        monitor = Popen(['/usr/bin/env', 'rosrun', 'rvmonitor', name])
-        def cleanup():
-            monitor.terminate()
-        request.addfinalizer(cleanup)
-        return monitor
-    return launch
+    def run_launch_monitor(monitor_name):
+        if(request.param):
+            monitor = Popen(['/usr/bin/env', 'rosrun', 'rvmonitor', monitor_name])
+            def cleanup():
+                monitor.terminate()
+            request.addfinalizer(cleanup)
+            return True
+        else:
+            return False
+    return run_launch_monitor
 
-## Tests without RVMaster
+# Tests
+def test_roscore__unmonitored_channel_pipeline(simple_pipeline):
+    test_packets = [StringPacket('/unmonitored', ['Hi!'],  '')]
+    simple_pipeline(test_packets)
+    assert ['Hi!'] == test_packets[0].recieved
 
-def to_formatted_string__String(msg):
-    return msg.data
+def check(with_monitor, expected, actual):
+    if(with_monitor):
+        assert expected == actual
+    else:
+        assert [] == actual
 
-def test_roscore__unmonitored_channel(ros_subscribe):
-    [ros_init, rate, recieved_messages] = ros_subscribe('unmonitored', String, to_formatted_string__String)
-    check_call(['rostopic', 'pub', '--once', '/unmonitored', 'std_msgs/String', 'Hi!'])
-    rate.sleep(); rate.sleep(); rate.sleep(); rate.sleep()
-    assert(recieved_messages == ['Hi!'])
+prefix = 'rv/monitored'
 
-def test_roscore__monitored_channel__no_monitor_running(ros_subscribe):
-    [ros_init, rate, recieved_messages] = ros_subscribe('monitored', String, to_formatted_string__String)
-    check_call(['rostopic', 'pub', '--once', '/chatter', 'std_msgs/String', 'Hi!'])
-    rate.sleep(); rate.sleep(); rate.sleep(); rate.sleep()
-    assert(recieved_messages == [])
+def test_roscore__monitored_channel(simple_pipeline, launch_monitor):
+    with_monitor = launch_monitor('monitor-single-parameter')
+    test_packets = [StringPacket('/chatter', ['Hi!', 'Hi!'], prefix)]
+    simple_pipeline(test_packets)
+    check(with_monitor, ['Hi!RV1', 'Hi!RV2'], test_packets[0].recieved)
 
-def test_roscore__monitored_channel__monitor_running(ros_subscribe, launch_monitor):
-    [ros_init, rate, recieved_messages] = ros_subscribe('chatter', String, to_formatted_string__String)
-    launch_monitor('monitor-single-parameter')
-    check_call(['rostopic', 'pub', '--once', '/rv/monitored/chatter', 'std_msgs/String', 'Hi!'])
-    check_call(['rostopic', 'pub', '--once', '/rv/monitored/chatter', 'std_msgs/String', 'Hi!'])
-    rate.sleep(); rate.sleep(); rate.sleep(); rate.sleep()
-    assert(recieved_messages == ['Hi!RV1', 'Hi!RV2'])
-
-def to_formatted_string__colorRGBA(msg):
-    return '({},{},{},{})'.format(msg.r, msg.g, msg.b, msg.a)
-
-def test_roscore__monitored_multiparam_channel__no_monitor_running(ros_subscribe, launch_monitor):
-    [ros_init, rate, recieved_messages] = ros_subscribe('color_chatter', ColorRGBA, to_formatted_string__colorRGBA)
-    launch_monitor('monitor-multiple-parameters')
-    test_msg = ColorRGBA(100, 100, 100, 0.5)
-    check_call(['rostopic', 'pub', '--once', '/color_chatter', 'std_msgs/ColorRGBA', str(test_msg)])
-    rate.sleep(); rate.sleep(); rate.sleep(); rate.sleep()
-    assert(recieved_messages == ['(100.0,100.0,100.0,0.5)'])
-
-def test_roscore__monitored_multiparam_channel__monitor_running(ros_subscribe, launch_monitor):
-    [ros_init, rate, recieved_messages] = ros_subscribe('color_chatter', ColorRGBA, to_formatted_string__colorRGBA)
-    launch_monitor('monitor-multiple-parameters')
-    test_msg = ColorRGBA(100, 100, 100, 0.5)
-    check_call(['rostopic', 'pub', '--once', '/rv/monitored/color_chatter', 'std_msgs/ColorRGBA', str(test_msg)])
-    rate.sleep(); rate.sleep(); rate.sleep(); rate.sleep()
-    assert(recieved_messages == ['(105.0,110.0,115.0,0.5)'])
-
-
-
+def test_roscore__monitored_multiparam_channel(simple_pipeline, launch_monitor):
+    with_monitor = launch_monitor('monitor-multiple-parameters')
+    test_packets = [ColorRGBAPacket('/color_chatter', [str(ColorRGBA(100 ,100 ,100 ,0.5))], prefix)]
+    simple_pipeline(test_packets)
+    check(with_monitor, ['(105.0,110.0,115.0,0.5)'], test_packets[0].recieved)
 
