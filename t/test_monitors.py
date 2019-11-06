120f6cabe49aac44cab768417fc69f2e2bf5c923
Manasvi Saxena <msaxena2@illinois.edu>
2019-11-06 15:45:39 -0800

    t/test_monitors.py: Single callback for each topic

--- t/test_monitors.py
+++ t/test_monitors.py
@@ -12,18 +12,16 @@ import pytest
 # ======================
 
 class RosTestPacket(object):
-    def __init__(self, topic_name, msg_class, msg_type, to_send):
+    def __init__(self, topic_name, msg_class, msg_type, to_send, callback):
         self.topic_name = topic_name
         self.msg_class = msg_class
         self.msg_type = msg_type
         self.to_send = to_send
-        self.recieved = []
+        self.callback = callback
 
-    def to_formatted_string(self, msg):
+    @classmethod
+    def to_formatted_string(cls, msg):
         pass
-
-    def callback(self, msg):
-        self.recieved += [self.to_formatted_string(msg)]
 
     def stamp_msg(self, msg):
         return msg
@@ -33,27 +31,30 @@ class RosTestPacket(object):
 # ====================
 
 class StringPacket(RosTestPacket):
-    def __init__(self, topic_name, to_send):
-        super(StringPacket, self).__init__(topic_name, String, 'std_msgs/String', to_send)
+    def __init__(self, topic_name, to_send, callback):
+        super(StringPacket, self).__init__(topic_name, String, 'std_msgs/String', to_send, callback)
 
-    def to_formatted_string(self, msg):
+    @classmethod
+    def to_formatted_string(cls, msg):
         return msg.data
 
 
 class ColorRGBAPacket(RosTestPacket):
-    def __init__(self, topic_name, to_send):
-        super(ColorRGBAPacket, self).__init__(topic_name, ColorRGBA, 'std_msgs/ColorRGBA', to_send)
+    def __init__(self, topic_name, to_send, callback):
+        super(ColorRGBAPacket, self).__init__(topic_name, ColorRGBA, 'std_msgs/ColorRGBA', to_send, callback)
 
-    def to_formatted_string(self, msg):
+    @classmethod
+    def to_formatted_string(cls, msg):
         return '({},{},{},{})'.format(msg.r, msg.g, msg.b, msg.a)
 
 
 class Float32Packet(RosTestPacket):
 
-    def __init__(self, topic_name, to_send):
-        super(Float32Packet, self).__init__(topic_name, Float32, 'std_msgs/Float32', to_send)
+    def __init__(self, topic_name, to_send, callback):
+        super(Float32Packet, self).__init__(topic_name, Float32, 'std_msgs/Float32', to_send, callback)
 
-    def to_formatted_string(self, msg):
+    @classmethod
+    def to_formatted_string(cls, msg):
         return "{0:0.1f}".format(msg.data)
 
     def stamp_msg(self, msg):
@@ -61,11 +62,11 @@ class Float32Packet(RosTestPacket):
 
 class Float32StampedPacket(RosTestPacket):
 
-    def __init__(self, topic_name, to_send):
-        super(Float32StampedPacket, self).__init__(topic_name, Float32Stamped,
-                'marti_common_msgs/Float32Stamped', to_send)
+    def __init__(self, topic_name, to_send, callback):
+        super(Float32StampedPacket, self).__init__(topic_name, Float32Stamped, 'marti_common_msgs/Float32Stamped', to_send, callback)
 
-    def to_formatted_string(self, msg):
+    @classmethod
+    def to_formatted_string(cls, msg):
         return "{0:0.1f}".format(msg.value)
 
     def stamp_msg(self, msg):
@@ -103,15 +104,18 @@ def session_init(get_options, launch_master):
 @pytest.fixture()
 def simple_pipeline(request, session_init, get_options):
     def run_simple_pipeline(test_packets):
+        registered_topics = []
         if('unmonitored_channel' in request.node.name):
             prefix = ''
         else:
             prefix = get_options['topic_prefix']
         rate = ros.Rate(10);
         for packet in test_packets:
-            # register callbacks
-            ros.Subscriber(packet.topic_name, packet.msg_class, packet.callback)
-            rate.sleep()
+            # register callbacks. Don't re-register new callbacks for existing topics
+            if not packet.topic_name in registered_topics:
+                ros.Subscriber(packet.topic_name, packet.msg_class, packet.callback.as_callable)
+                registered_topics.append(packet.topic_name)
+                rate.sleep()
             for unstamped_msg in packet.to_send:
                 stamped_msg = packet.stamp_msg(unstamped_msg)
                 check_call(['rostopic', 'pub', '--once'
@@ -131,76 +135,105 @@ def launch_monitor(request, get_options):
         return True
     return run_launch_monitor
 
+class CallBack(object):
+    def __init__(self, to_formatted_string):
+        self.recieved_list = []
+        self.to_formatted_string = to_formatted_string
+
+    def as_callable(self, msg):
+        self.recieved_list.append(self.to_formatted_string(msg))
+
 
 # Basic Tests
 # ===========
 
 def test_roscore__unmonitored_channel_pipeline(simple_pipeline):
-    test_packets = [StringPacket('/unmonitored', ['Hi!'])]
-    simple_pipeline(test_packets)
-    assert ['Hi!'] == test_packets[0].recieved
+    with capsys.disabled():
+        string_callback = CallBack(StringPacket.to_formatted_string)
+        test_packets = [StringPacket('/unmonitored', ['Hi!'], string_callback)]
+        simple_pipeline(test_packets)
+        assert ['Hi!'] == string_callback.recieved_list
 
 def test_roscore__monitored_channel__no_monitor_running(simple_pipeline):
-    test_packets = [StringPacket('/chatter', ['Hi!', 'Hi!'])]
+    string_callback = CallBack(StringPacket.to_formatted_string)
+    test_packets = [StringPacket('/chatter', ['Hi!', 'Hi!'], string_callback)]
     simple_pipeline(test_packets)
-    assert([] == test_packets[0].recieved)
+    assert([] == string_callback.recieved_list)
 
 def test_roscore__monitored_channel(simple_pipeline, launch_monitor):
     launch_monitor('monitor-single-parameter')
-    test_packets = [StringPacket('/chatter', ['Hi!', 'Hi!'])]
+    string_callback = CallBack(StringPacket.to_formatted_string)
+    test_packets = [StringPacket('/chatter', ['Hi!', 'Hi!'], string_callback)]
     simple_pipeline(test_packets)
-    assert(['Hi!RV1', 'Hi!RV2'] == test_packets[0].recieved)
+    assert(['Hi!RV1', 'Hi!RV2'] == string_callback.recieved_list)
 
 def test_roscore__monitored_multiparam_channel(simple_pipeline, launch_monitor):
     launch_monitor('monitor-multiple-parameters')
-    test_packets = [ColorRGBAPacket('/color_chatter', [str(ColorRGBA(100 ,100 ,100 ,0.5))])]
+    color_rgba_callback = CallBack(ColorRGBAPacket.to_formatted_string)
+    test_packets = [ColorRGBAPacket('/color_chatter', [str(ColorRGBA(100 ,100 ,100 ,0.5))], color_rgba_callback)]
     simple_pipeline(test_packets)
-    assert(['(105.0,110.0,115.0,0.5)'] == test_packets[0].recieved)
+    assert(['(105.0,110.0,115.0,0.5)'] == color_rgba_callback.recieved_list)
 
 def test_roscore__monitored_multiparam_multichannel(simple_pipeline, launch_monitor):
     launch_monitor('monitor-multiple-channels')
-    test_packets = [ ColorRGBAPacket('/color_chatter', [str(ColorRGBA(100 ,100 ,100 ,0.5))])
-                   , StringPacket('/chatter', ['Hi!'])]
+    color_rgba_callback = CallBack(ColorRGBAPacket.to_formatted_string)
+    string_callback = CallBack(StringPacket.to_formatted_string)
+    test_packets = [ ColorRGBAPacket('/color_chatter', [str(ColorRGBA(100 ,100 ,100 ,0.5))], color_rgba_callback)
+                   , StringPacket('/chatter', ['Hi!'], string_callback)]
     simple_pipeline(test_packets)
-    assert(['(105.0,110.0,115.0,0.5)'] == test_packets[0].recieved)
-    assert(['Hi!RV']                   == test_packets[1].recieved)
+    assert(['(105.0,110.0,115.0,0.5)'] == color_rgba_callback.recieved_list)
+    assert(['Hi!RV']                   == string_callback.recieved_list)
 
 # Dl Tests
 # ========
 
-# TODO: Replace instance callback on shared topics with static callbacks
 @pytest.mark.dlTest
 def test_roscore__monitored_dl_watertank_unsafe(simple_pipeline, launch_monitor):
-   h = ros.Header()
-   h.stamp = ros.Time.now()
-   launch_monitor('monitor-dl-watertank')
-   sensor_packets = [ Float32Packet( '/level_sensor', [x]) for x in [0.0, 0.0] ]
-   control_packets = [ Float32StampedPacket( '/flow_control_cmd', [Float32Stamped(h, x)]) for x in [0.0, 0.7] ]
-   simple_pipeline( [ sensor_packets[0], control_packets[0]
-                , sensor_packets[1], control_packets[1] ])
+    h = ros.Header()
+    h.stamp = ros.Time.now()
+    launch_monitor('monitor-dl-watertank')
+    sensor_callback = CallBack(Float32Packet.to_formatted_string)
+    sensor_packets = [ Float32Packet( '/level_sensor', [x], sensor_callback)
+                         for x in [0.0, 0.0] ]
 
-   rate = ros.Rate(10)
-   rate.sleep(); rate.sleep(); rate.sleep(); rate.sleep()
-   # Unsafe Control Changed to safe control
-   assert(control_packets[1].recieved[-1] == '0.0')
+    control_callback = CallBack(Float32StampedPacket.to_formatted_string)
+    control_packets = [ Float32StampedPacket( '/flow_control_cmd', [Float32Stamped(h, x)], control_callback)
+                         for x in [0.0, 0.7] ]
+
+    simple_pipeline( [ sensor_packets[0], control_packets[0]
+                   ,   sensor_packets[1], control_packets[1] ] )
+
+    rate = ros.Rate(10)
+    rate.sleep(); rate.sleep(); rate.sleep(); rate.sleep()
+
+    # Unsafe Control Changed to safe control
+    assert(['0.0', '0.0'] == control_callback.recieved_list)
+
+    # Sensor Readings Unchanged
+    assert(['0.0', '0.0'] == sensor_callback.recieved_list)
 
 @pytest.mark.dlTest
 def test_roscore__monitored_dl_watertank_safe_after_unsafe(simple_pipeline, launch_monitor):
     h = ros.Header()
     h.stamp = ros.Time.now()
     launch_monitor('monitor-dl-watertank')
-    sensor_packets = [ Float32Packet( '/level_sensor', [x]) for x in [0.0, 0.0, 0.0] ]
+    sensor_callback = CallBack(Float32Packet.to_formatted_string)
+    sensor_packets = [ Float32Packet( '/level_sensor', [x], sensor_callback)
+                        for x in [0.0, 0.0, 0.0] ]
 
-    control_packets = [ Float32StampedPacket( '/flow_control_cmd', [Float32Stamped(h, x)]) for x in [0.0, 0.5, 0.1] ]
+    control_callback = CallBack(Float32StampedPacket.to_formatted_string)
+    control_packets = [ Float32StampedPacket( '/flow_control_cmd', [Float32Stamped(h, x)], control_callback)
+                        for x in [0.0, 0.5, 0.1] ]
+
     simple_pipeline( [ sensor_packets[0], control_packets[0]
                 , sensor_packets[1], control_packets[1]
                 , sensor_packets[2], control_packets[2] ])
     rate = ros.Rate(10)
     rate.sleep(); rate.sleep(); rate.sleep(); rate.sleep()
 
-    # Unsafe Control Changed to safe control
-    assert(control_packets[1].recieved[1] == '0.0')
+    # Unsafe Control to safe (fallback) control and safe (blackbox) control
+    assert(['0.0', '0.0', '0.1'] == control_callback.recieved_list)
 
-    # Safe Control resumption after unsafe control
-    assert(control_packets[2].recieved[-1] == '0.1')
+    # Sensor Readings Unchanged
+    assert(['0.0', '0.0', '0.0'] == sensor_callback.recieved_list)
 
