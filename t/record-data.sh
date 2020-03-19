dfc64178e96bfc6cd8df47e86a397b2aafda1333
Stephen Skeirik <stephen.skeirik@runtimeverification.com>
2020-03-19 11:38:48 -0500

    update record-data output filename for consistency

--- t/record-data.sh
+++ t/record-data.sh
@@ -12,12 +12,12 @@ TEST_NAME=$1
 NET_IFACE=$2
 IP_B=$3
 DATA_FILE="$TEST_NAME.pcap"
-RESULT_FILE="$TEST_NAME-results.txt"
+RESULT_FILE="$TEST_NAME-results.log"
 
 echo "Recording data..."
 sudo tshark -i "$NET_IFACE" -f "tcp and host $IP_B" -w "/tmp/$DATA_FILE"
 echo "Saving data..."
-sudo chmod a+rwx "/tmp/$DATA_FILE"
+sudo chmod a+rw "/tmp/$DATA_FILE"
 cp "/tmp/$DATA_FILE" "$DIR"
 echo "Recording results..."
 tshark -r "$DIR/$DATA_FILE" -z io,stat,0,"SUM(frame.len)frame.len" > "$DIR/$RESULT_FILE"
